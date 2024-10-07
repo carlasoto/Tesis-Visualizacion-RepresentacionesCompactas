ab32ce2942fefeab7a4132f8e533d8a557e49677
Carla Soto <42447983+carlasoto@users.noreply.github.com>
2024-10-06 23:40:03 -0300

    Pruebas_Automatizadas.sh

--- Programa/Pruebas_Automatizadas.sh
+++ Programa/Pruebas_Automatizadas.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-CURRENT_DIR=$(pwd)
+# Obtener la ruta del directorio donde se encuentra el script
+SCRIPT_DIR=$(dirname "$0")
 
-#Modificar aqui por la versión local que se tenga del ambiente de python de Irri
-PYTHON_PATH="/home/casoto/tesis/pysdsl/./envPython/python/install/bin/python3"
+# Solicitar al usuario la ruta del Python que desea utilizar
+read -p "Ingresar ruta del ambiente Python: " PYTHON_PATH
 
-# Número de veces a ejecutar el script
-times=5
+while [ ! -x "$PYTHON_PATH" ]; do
+    echo "Ruta inválida."
+    read -p "Ingresar ruta del ambiente Python: " PYTHON_PATH
+done
+
+read -p "Cantidad de pruebas a realizar: " times
+
+while ! [[ "$times" =~ ^[0-9]+$ && "$times" -gt 0 ]]
+do
+    echo "Inválido."
+    read -p "Cantidad de puebas a realizas: " times
+done
 
 for ((i=1; i<=times; i++))
 do
@@ -14,7 +25,8 @@ do
    
    echo "Hora antes de la ejecución: $(date '+%Y-%m-%d %H:%M:%S')"
 
-   $PYTHON_PATH "$CURRENT_DIR/Auto_main.py"
+   # Ejecutar el archivo Auto_main.py ubicado en la misma carpeta que este script
+   $PYTHON_PATH "$SCRIPT_DIR/Auto_main.py"
 
    echo "Hora después de la ejecución: $(date '+%Y-%m-%d %H:%M:%S')"
 done
